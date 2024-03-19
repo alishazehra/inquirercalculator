725dc69ca8f4a1f24ef5f5d7478af7540ffc3df9
Alisha Zehra <161215836+alishazehra@users.noreply.github.com>
2024-03-19 15:36:25 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {message: "Enter first number", type:"number", name: "firstNumber"},
+    {message: "Enter second number", type: "number", name:"secondnumber"},
+    {message: "Select one of the operators to perform action",
+    type: "list" ,
+    name:"operator" ,
+    choices:["Addition", "Subtraction", "Multiplication", "Division"]}
+    ]);
+// Conditional statements
+if (answer.operator === "Addition"){
+    console.log("Your value is + answer.firstNumber + answer.secondNumber")
+}
+
